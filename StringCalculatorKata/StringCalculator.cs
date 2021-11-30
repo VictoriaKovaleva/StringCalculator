12995f52d01c54cb01ac1a3782ed38691a148908
vkovaleva <vkovaleva@voxelgroup.net>
2021-11-30 18:11:32 +0100

    refactor: simplify Sum method

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -8,10 +8,8 @@ namespace StringCalculatorKata
         {
             if (String.IsNullOrEmpty(numbers))
                 return 0;
-            if (Convert.ToInt32(numbers) == 2)
-                return 2;
-           
-            return 1;
+
+            return (Convert.ToInt32(numbers));
         }
     }
 }
