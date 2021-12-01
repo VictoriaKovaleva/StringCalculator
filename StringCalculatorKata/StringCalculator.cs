5746e0db8854439354fdefbf534af663f2070b64
vkovaleva <vkovaleva@voxelgroup.net>
2021-12-01 17:46:24 +0100

    refactor: generalize condition for a negative number

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -10,19 +10,20 @@ namespace StringCalculatorKata
             if (String.IsNullOrEmpty(numbers))
                 return 0;
 
-            if (numbers == "-2")
-                throw new ArgumentException("negatives not allowed -2", nameof(numbers));
-
             var sum = 0;
             var numbersArray = numbers.Replace("\n", ",").Split(",");
 
             if (numbers.Contains(";"))
                 numbersArray = numbers.Substring(numbers.IndexOf('\n') + 1).Split(";");
 
-            
             foreach (var number in numbersArray)
+            {
+                if (number.StartsWith("-"))
+                    throw new ArgumentException("negatives not allowed" + number, nameof(numbers));
+                
                 sum += int.Parse(number);
-            
+            }
+
             return sum;
         }
     }
